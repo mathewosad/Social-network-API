2308306dac214500277d318a9819e710ea2099e7
mathewosad <96810252+mathewosad@users.noreply.github.com>
2022-03-22 19:19:04 -0700

    fixing routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,9 @@
+// connecting routes to the server
 const router = require('express').Router();
-const courseRoutes = require('./courseRoutes');
-const studentRoutes = require('./studentRoutes');
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 
-router.use('/courses', courseRoutes);
-router.use('/students', studentRoutes);
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
